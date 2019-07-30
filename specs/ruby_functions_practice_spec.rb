f033a6befb307b4c7a46c7cfc804896f6aa4095c
Ian Shepherd <ianshepherd77@gmail.com>
2019-07-30 21:00:25 +0100

    Amendment to homework submission

--- specs/ruby_functions_practice_spec.rb
+++ specs/ruby_functions_practice_spec.rb
@@ -1,11 +1,47 @@
-require('minitest/autorun')
-require('minitest/rg')
-require_relative('../ruby_functions_practice')
+num2 = num2.to_i
+return num1 + num2
+end
 
-class FunctionsTest < MiniTest::Test
+def number_to_full_month_name(num)
+case num
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  else
+    return "nil"
+end
+end
 
-  def test_return_10
-    result = return_10()
-    assert_equal(10, result)
-  end
+
+
+def number_to_short_month_name(num)
+case num
+  when 1
+    return "Jan"
+  when 4
+    return "Apr"
+  when 10
+    return "Oct"
+  else
+    return nil
+end
+end
+
+
+def volume_of_cube(length, width, height)
+return length * width * height
+end
+
+
+def volume_of_sphere(radius)
+result=3.14 * 4/3 * radius**3
+return result.to_i
+end
+
+def fahrenheit_to_celsius(fahrenheit)
+ celsius = (fahrenheit - 32) * 5/9
+ return celsius.to_i
 end
